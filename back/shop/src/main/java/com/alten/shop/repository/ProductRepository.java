4dbe2b46212fb891a991a444ce4ef2354483a768
testhicham50 <156068511+testhicham50@users.noreply.github.com>
2024-01-18 00:01:37 +0100

    Update ProductRepository.java

--- back/shop/src/main/java/com/alten/shop/repository/ProductRepository.java
+++ back/shop/src/main/java/com/alten/shop/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    List<Product> findByCategory(String category);
+    
+    Optional<Product> findByCode(String id);
 
 }
